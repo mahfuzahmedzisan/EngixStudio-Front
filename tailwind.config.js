7241358c35a705d0ef2b783b04e4efcedca6f668
Mahfuz Ahmed Zisan <mahfuz.ahmed.zisan@gmail.com>
2024-12-05 21:04:14 +0600

    update navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./src/**/*.{html,js}", "*"],
   theme: {
     extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+      },
+      screens: {
+        '2xl': '1400px',
+      },
+    }
   },
   plugins: [
     require('daisyui'),
