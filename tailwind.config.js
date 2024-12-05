13ca53c746b53979f4333bf9aacda253bb8dedcd
Mahfuz Ahmed Zisan <mahfuz.ahmed.zisan@gmail.com>
2024-12-05 21:34:21 +0600

    Up date Header for Desktop

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,33 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "*"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xs': '375px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1300px',
+      },
+      colors: {
+        'c-primary': '#7C60A6',
+        'c-secondary': '#4951A5',
+        'c-tertiary': '#F4F4F4',
+
+        't-primary': '#75f',
+        't-black': '#070709',
+        't-white': '#FFFFFF',
+        't-gray': '#A0A0A0',
+      }
+    },
     container: {
       center: true,
       padding: {
         DEFAULT: '1rem',
       },
       screens: {
-        '2xl': '1400px',
+        '2xl': '1300px',
       },
     }
   },
