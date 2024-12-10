cbfe1de61c2a837d410fda6e6fda36ac1feb710a
Mahfuz Ahmed Zisan <mahfuz.ahmed.zisan@gmail.com>
2024-12-10 16:07:31 +0600

    Add About Page
    Hero Section --done
    
    Slogan section --done
    
    story section --done
    
    team members section --done

---
+++ src/js/teamMember.js
@@ -0,0 +1,23 @@
+function adjustPadding() {
+   const members = Array.from(document.querySelectorAll('#team-grid > div'));
+   members.forEach(member => member.style.paddingTop = '0'); // Reset padding
+
+   if (window.innerWidth >= 768) {
+      // Apply padding to middle member of each row with 3 members
+      members.forEach((member, index) => {
+         if (index % 3 === 1) {
+            member.style.paddingTop = '4rem';
+         }
+      });
+   } else if (window.innerWidth >= 480) {
+      // Apply padding to second member in rows with 2 members
+      members.forEach((member, index) => {
+         if (index % 2 === 1) {
+            member.style.paddingTop = '4rem';
+         }
+      });
+   }
+}
+
+window.addEventListener('load', adjustPadding);
+window.addEventListener('resize', adjustPadding);
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     extend: {
       screens: {
         'xs': '375px',
+        '2xs': '480px',
         'sm': '640px',
         'md': '768px',
         'lg': '1024px',
